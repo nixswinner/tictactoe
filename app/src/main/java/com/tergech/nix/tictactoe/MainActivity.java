446e299d1d9aa69eac2dfcae759a1a7ccaa9046d
nixswinner <nixswinner@gmail.com>
2018-04-10 21:39:35 +0300

    improving on Ui and added the scoreboard

--- app/src/main/java/com/tergech/nix/tictactoe/MainActivity.java
+++ app/src/main/java/com/tergech/nix/tictactoe/MainActivity.java
@@ -1,7 +1,11 @@
 package com.tergech.nix.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView three = findViewById(R.id.three);
+        three.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent threeIntent = new Intent(MainActivity.this,_3by3_board.class);
+                startActivity(threeIntent);
+
+            }
+        });
+
+        TextView five = findViewById(R.id.five);
+        five.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent fiveIntent = new Intent(MainActivity.this,_5by5_board.class);
+                startActivity(fiveIntent);
+
+            }
+        });
+        Button exit =findViewById(R.id.exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
